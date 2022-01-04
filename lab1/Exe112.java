14800c804efc9c86a59ad7fc9c6b276efe14e501
fernweh-g <fernweh204659@gmail.com>
2022-01-04 21:17:54 +0800

    complete first part of lab1

---
+++ lab1/Exe112.java
@@ -0,0 +1,13 @@
+package com.CS61B.Essential;
+
+public class Exe112 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 0;
+        while (i<=9){
+            sum = sum + i;
+            i++;
+            System.out.print(sum + " ");
+        }
+    }
+}
